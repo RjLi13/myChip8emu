f04152c599ea70dc1944f0763d144969ebb4fc72
Ruijing Li <ruijing.li@berkeley.edu>
2018-09-06 13:08:33 -0700

    Starting work after reading tutorial

--- Chip8emu/chip8.cpp
+++ Chip8emu/chip8.cpp
@@ -11,11 +11,34 @@
 void Chip8::initialize()
 {
     // Initialize registers and memory once
-    pc = 0x200;
-    opcode = 0;
-    I = 0;
-    sp = 0;
+    pc = 0x200; // program counter starts at 0x200
+    opcode = 0; // reset curr opcode
+    I = 0; // reset index register
+    sp = 0; // reset stack pointer
     
+    // Clear display
+    // Clear stack
+    // Clear registers V0-VF
+    // Clear memory
+    
+    // Load fontset
+    // This fontset should be loaded in memory location 0x50 == 80
+    for(int i = 0; i < 80; ++i)
+    {
+        memory[i] = chip8_fontset[i];
+    }
+    
+    // Reset Timers
+}
+
+void Chip8::loadGame(<#std::string#> prog)
+{
+    /*
+     load the program into the memory (use fopen in binary mode) and start filling the memory at
+     location: 0x200 == 512.
+     */
+    for(int i = 0; i < bufferSize; ++i)
+        memory[i+512] = buffer[i];
 }
 
 void Chip8::emulateCycle()
@@ -29,15 +52,52 @@ void Chip8::emulateCycle()
     
     // Decode Opcode
     // check the opcode table to see what it means.
-    
-    // Execute Opcode
-    /*
-     * Because every instruction is 2 bytes long, we need to increment the program counter by two after
-     * every executed opcode. This is true unless you jump to a certain address in the memory or if you
-     * call a subroutine (in which case you need to store the program counter in the stack). If the next
-     * opcode should be skipped, increase the program counter by four.
-     */
+    switch(opcode & 0xF000)
+    {
+        case 0xA000: // ANNN: Sets I to address NNN
+            // Execute Opcode
+            I = opcode & 0x0FFF;
+            /*
+             * Because every instruction is 2 bytes long, we need to increment the program counter by
+             * two after
+             * every executed opcode. This is true unless you jump to a certain address in the memory or
+             * if you
+             * call a subroutine (in which case you need to store the program counter in the stack). If
+             * the next
+             * opcode should be skipped, increase the program counter by four.
+             */
+            pc += 2;
+            break;
+            
+        case 0x0000:
+            switch(opcode & 0x000F)
+            {
+                case 0x0000: // 0x00E0: Clears screen
+                    break;
+                    
+                case 0x000E: //0x00EE: Returns from subroutine
+                    break;
+                    
+                default:
+                    printf ("Unknown opcode [0x0000]: 0x%X\n", opcode);
+            }
+            break;
+            
+        default:
+            printf ("Unknown opcode: 0x%X\n", opcode);
+    }
+
+
     // Update timers
     /* both timers count down to zero if they have been set to a value larger than zero. Since these timers count down at 60 Hz, you might want to implement something that slows down your emulation cycle (Execute 60 opcodes in one second).
      */
+    if(delay_timer > 0)
+        --delay_timer;
+    
+    if(sound_timer > 0)
+    {
+        if(sound_timer == 1)
+            printf("BEEP!\n");
+        --sound_timer;
+    }
 }
